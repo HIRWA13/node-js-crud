6666704b74224b5625a1b690200fb230a568ca24
Junior Hirwa <iamhirwejr@gmail.com>
2024-03-04 12:12:13 +0200

    feat: configure app

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const itemsRouter = require('./routes/itemRoutes')
+// require the .env
+require('dotenv').config()
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const dbURI = process.env.DBURI
+
+mongoose
+  .connect(dbURI)
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
+
+
+
+app.get('/', (req, res) =>{
+    res.redirect('/items')
+})
+
+app.get('/items/create', (req, res) => {
+    res.render('items/create', { title: 'Create a new item'})
+})
+
+app.get('/about', (req, res) => {
+  res.render('items/about', { title: 'About us'})
+})
+
+app.use(itemsRouter)
